10c566cf5cbdbf67a601bc7f7aeed75c9be96a01
Konrad Koziński <konrad.kozinski@energa.pl>
2014-12-04 16:45:18 +0100

    First Commit

---
+++ Foxteca/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Foxteca
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+            name: "Testval",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Page", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+            name: "AboutRoute",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Page", action = "About", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
